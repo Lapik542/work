18877971b32db8df7653081d6437a2c704df1d79
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-11-22 19:15:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -693,3 +693,54 @@
 // console.log(make);
 // // console.log(result);
 
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return balance;
+//   },
+//   getDiscount() {
+//     return discount;
+//   },
+//   setDiscount(value) {
+//     discount = value;
+//   },
+//   getOrders() {
+//     return orders;
+//   },
+//   addOrder(cost, order) {
+//     balance -= cost - cost * discount;
+//     orders.push(order);
+//   },
+//   // Change code above this line
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+const shopStone = {
+   stones: [
+      { name: 'Emerald', price: 1300, quantity: 4 },
+      { name: 'Diamond', price: 2700, quantity: 3 },
+      { name: 'Sapphire', price: 1400, quantity: 7 },
+      { name: 'Ruby', price: 800, quantity: 2 },
+   ],
+   calcTotalPrice(stoneName) {
+      const foundStone = this.stones.find(stone => stone.name === stoneName);
+
+      if (!foundStone) return 0;
+
+      return foundStone.price * foundStone.quantity;
+   },
+}; 
+
+console.log(shopStone.calcTotalPrice('Emerald'));
+console.log(shopStone.calcTotalPrice('Diamond'));
+console.log(shopStone.calcTotalPrice('Sapphire'));
+console.log(shopStone.calcTotalPrice('Ruby'));
