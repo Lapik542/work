720ce45acb4667cd7fdd0a4f707ef17e5b06147c
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-12-07 11:49:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1079,3 +1079,23 @@
 // console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 // storage.removeItem("Prolonger");
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
+const list = document.querySelector('#categories');
+const childeList = list.children;
+console.log(`Number of categories: ${childeList.length}`);
+
+const animals = list.querySelector('h2');
+console.log(`Category: ${animals.textContent}`);
+const elementAnimal = childeList[0].querySelectorAll('li');
+console.log(`Elements: ${elementAnimal.length}`);
+
+const product = list.querySelectorAll('#categories>li>h2')
+console.log(`Category: ${product[1].textContent}`);
+const elementProduct = childeList[1].querySelectorAll('li')
+console.log(`Elements: ${elementProduct.length}`);
+
+const technologies = list.querySelectorAll('#categories>li>h2')
+console.log(`Category: ${technologies[2].textContent}`);
+const elementTechnologies = childeList[2].querySelectorAll('li')
+console.log(`Elements: ${elementTechnologies.length}`);
