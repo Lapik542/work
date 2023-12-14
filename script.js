a63180a4731996855979765f02ccb5eb308f0479
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-12-14 18:49:44 +0200

    main

--- script.js
+++ script.js
@@ -1244,23 +1244,72 @@
 
 
 
-const form = document.querySelector('.login-form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+// const form = document.querySelector('.login-form');
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
 
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
+//   const emailValue = email.value.trim();
+//   const passwordValue = password.value.trim();
 
-  if (emailValue === '' || passwordValue === '') {
-    alert('All form fields must be filled in');
-  } else {
-    const formData = {
-      email: emailValue,
-      password: passwordValue
-    };
+//   if (emailValue === '' || passwordValue === '') {
+//     alert('All form fields must be filled in');
+//   } else {
+//     const formData = {
+//       email: emailValue,
+//       password: passwordValue
+//     };
 
-    console.log(formData);
+//     console.log(formData);
 
-    this.reset();
+//     this.reset();
+//   }
+// });
+
+
+import Calendar from "color-calendar";
+import "color-calendar/dist/css/theme-glass.css";
+import "color-calendar/dist/css/theme-basic.css";
+
+let calA = new Calendar({
+  id: "#calendar-a",
+  theme: "glass",
+  // border: "5px solid black",
+  weekdayType: "long-upper",
+  monthDisplayType: "long",
+  // headerColor: "yellow",
+  // headerBackgroundColor: "black",
+  calendarSize: "small",
+  layoutModifiers: ["month-left-align"],
+  eventsData: [
+    {
+      id: 1,
+      name: "French class",
+      start: "2020-12-17T06:00:00",
+      end: "2020-12-18T20:30:00"
+    },
+    {
+      id: 2,
+      name: "Blockchain 101",
+      start: "2020-12-20T10:00:00",
+      end: "2020-12-20T11:30:00"
+    },
+    {
+      id: 3,
+      name: "Cheese 101",
+      start: "2020-12-01T10:00:00",
+      end: "2020-12-02T11:30:00"
+    },
+    {
+      id: 4,
+      name: "Cheese 101",
+      start: "2020-12-01T10:00:00",
+      end: "2020-12-02T11:30:00"
+    }
+  ],
+  dateChanged: (currentDate, events) => {
+    console.log("date change", currentDate, events);
+  },
+  monthChanged: (currentDate, events) => {
+    console.log("month change", currentDate, events);
   }
 });
